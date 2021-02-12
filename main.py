6fab957bc80106b25914c00c9e004a1444cd4924
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-02-12 15:26:49 -0300

    news downloaded

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
+import csv
 import json
-url = 'https://www.metropoles.com/wp-json/metropoles/v1/last_news?&paged=1&category=182&limit=100'
+import sys
+import numpy as np
+
+url = 'https://www.metropoles.com/wp-json/metropoles/v1/last_news?&paged=1&category=182&limit=10000'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 result = requests.get(url, headers=headers)
 data = json.loads(result.content)
@@ -8,15 +12,24 @@ data = json.loads(result.content)
 i = (len(data))
 j = 0
 final_data = []
-print(data[0]['url'])
-"""
-for news in data:
-    x = news['excerpt'].find("Preso")
-    if(x!=-1):
-        final_data.append(news['url'])
-    print("Loading: ", j, " out of: ", i)
-    j = j + 1
+print(data[90]['url'])
 
+with open('news.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    for news in data:
+        #x = news['excerpt'].find("Preso")
+        #if(x!=-1):
+        #    final_data.append(news['url'])
+        result = news.items()
+
+        news_array = list(result)
+
+        an_array = np.array(news_array)
+
+        writer.writerow([an_array])
+        print("Loading: ", j, " out of: ", i)
+        j = j + 1
+
+print(type(news))
 print("Finded: ", len(final_data))
 
-"""
