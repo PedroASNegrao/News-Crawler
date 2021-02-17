44db818b824f0eac230f1e0f4bcea2ad78f1983e
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-02-17 20:55:25 -0300

    correio versão final

--- cvs_interations/csv_correio.py
+++ cvs_interations/csv_correio.py
@@ -8,13 +8,14 @@ correio_data = pd.read_csv('./../Data/news_correio.csv')
 data_array = correio_data.to_numpy()
 
 all_links = []
+all_dates = []
 words_finded = []
 i = 0
 objeto = []
 #print(data_array)
 
 #Palaras para serem pesquisadas:
-words = ["Polícia", "Policiais", "Policial", "Bandido", "Bandida", "Bandidos", "Criminoso", "Criminosa", "Criminosos", "Vítima", "Vítimas", "Crime", "Crimes", "Lei", "Legislação", "Penal", "Penais", "Contravenção", "Contravenções", "Infrações", "Infração", "Infrator", "Infratora", "Infratores", "Contraventor", "Contraventora", "Contraventores", "Advogado", "Advogada", "Advogados", "Juiz", "Juiza" ,"Juízo", "Ministério Público", "MP", "Delegacia", "Boletim de ocorrência", "Testemunhas", "Testemunha", "Testemunhou", "Tribunal", "Justiça", "DP"]
+words = ["Polícia", "Policiais", "Policial", "Bandido", "Bandida", "Bandidos", "Criminoso", "Criminosa", " MP ", "Criminosos", "Vítima", "Vítimas", "Crime", "Crimes", " Lei ", "Legislação", "Penal", "Penais", "Contravenção", "Contravenções", "Infrações", "Infração", "Infrator", "Infratora", "Infratores", "Contraventor", "Contraventora", "Contraventores", "Advogado", "Advogada", "Advogados", "Juiz", "Juiza", "Juízo", "Ministério Público", "Delegacia", "Boletim de ocorrência", "Testemunhas", "Testemunha", "Testemunhou", "Tribunal", "Justiça", " DP "]
 low_case_words = []
 
 for word in words:
@@ -24,7 +25,7 @@ words = words + low_case_words
 
 #----------------
 
-#url_test = ['https://www.correiobraziliense.com.br/cidades-df/2021/02/4907151-gdf-vai-construir-viaduto-para-facilitar-acesso-a-vicente-pires.html','https://www.correiobraziliense.com.br/cidades-df/2021/02/4907151-gdf-vai-construir-viaduto-para-facilitar-acesso-a-vicente-pires.html', 'https://www.correiobraziliense.com.br/cidades-df/2021/02/4907152-policia-militar-prende-dois-homens-em-flagrante-de-estupro-no-paranoa.html']
+url_test = ['https://www.correiobraziliense.com.br/cidades-df/2021/02/4905821-familiares-velam-corpo-de-menina-que-morreu-em-acidente-na-br-070.html','https://www.correiobraziliense.com.br/cidades-df/2021/02/4907163-terceiro-envolvido-na-morte-de-estudante-em-planaltina-se-entrega.html','https://www.correiobraziliense.com.br/cidades-df/2021/02/4907151-gdf-vai-construir-viaduto-para-facilitar-acesso-a-vicente-pires.html', 'https://www.correiobraziliense.com.br/cidades-df/2021/02/4907152-policia-militar-prende-dois-homens-em-flagrante-de-estupro-no-paranoa.html']
 
 #for link in url_test:
 for link in data_array:
@@ -44,30 +45,37 @@ for link in data_array:
 
     required = False
     cont_paragrafo = 0
-    objeto.clear()
 
-    for paragrafs in soup.find_all('p'):
+
+    for paragrafs in soup.find_all(attrs={"class": "texto"}):
         text = str(paragrafs.string)
         cont_paragrafo = cont_paragrafo + 1
         print("Checando paragrafo: %d" % cont_paragrafo)
         #print(text)
+        #
         for parameter in words:
             if text.find(parameter) != -1:
                 #print(text)
+                print("OK")
                 required = True
                 objeto.append(parameter)
 
-    words_finded.append(objeto)
     if(required == True):
+        words_finded.append(objeto)
+        objeto = []
         all_links.append(url)
+        all_dates.append(link[2])
         required = False
+
+
+
 
 
 
 print(all_links)
 print(words_finded)
 
-df = pd.DataFrame({'url': all_links, 'palavras-encontradas': words_finded})
+df = pd.DataFrame({'url': all_links, 'palavras-encontradas': words_finded, 'data': all_dates})
 #print(df)
 df.to_excel('./../Excel/news_correio2.xls', index=False, encoding='utf-8')
 #print(type(soup.p.string))
